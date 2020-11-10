e31a659c4a89e49d544ab33b1f2e7a72d64e7cd5
Robyn Tyler <robynstyler@outlook.com>
2020-11-10 17:12:57 +0000

    tested queue starts empty

--- bus_lab_start_code/tests/bus_stop_test.py
+++ bus_lab_start_code/tests/bus_stop_test.py
@@ -1,5 +1,5 @@
 import unittest
-# from src.bus_stop import BusStop
+from src.bus_stop import BusStop
 from src.person import Person
 
 class TestBusStop(unittest.TestCase):
@@ -7,11 +7,11 @@ class TestBusStop(unittest.TestCase):
         self.bus_stop = BusStop("Waverly Station")
         self.person = Person("Guido van Rossum", 64)
 
-    @unittest.skip("Delete this line to run the test")
+    # @unittest.skip("Delete this line to run the test")
     def test_bus_stop_has_name(self):
         self.assertEqual("Waverly Station", self.bus_stop.name)
 
-    @unittest.skip("Delete this line to run the test")
+    # @unittest.skip("Delete this line to run the test")
     def test_queue_starts_empty(self):
         self.assertEqual(0, self.bus_stop.queue_length())
 
