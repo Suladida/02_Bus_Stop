5de542f718101761e5c03cf82d5130c568934cc3
Robyn Tyler <robynstyler@outlook.com>
2020-11-10 16:28:01 +0000

    tested starts with no passengers

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -2,6 +2,12 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
+        self.passengers = []
+        
 
     def drive(self):
         return "Brum brum"
+    
+    def passenger_count(self):
+        passenger_count = len(self.passengers)
+        return passenger_count
